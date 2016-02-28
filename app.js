cfaf96977d9bf90e9d67b4ce515e787bbd18f362
ZudenSachenselbst <fumi_hana_momo@yahoo.co.jp>
2016-02-28 18:24:43 +0900

    first addition

---
+++ app.js
@@ -0,0 +1,32 @@
+/*
+ * hello, world
+ * IPなど設定：http://testcording.com/?p=1164
+ */
+/*
+ * パラメータ
+ */
+var IP_ADDRESS = "localhost";
+var PORT       = 1337;
+
+/*
+ * モジュール読み込み
+ */
+var http = require("http");
+
+/*
+ * サーバの作成
+ */
+http.createServer(function (req, res) {
+	// リクエストされたURLをログに表示(ブラウザからアクセスされるたびにコンソールへ出力する)
+	console.log(req.url);
+	// ヘッダーの送信 送信するデータのタイプはtext/plain
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	// 中身の文字列を送信
+	res.end("hello, world! : URL " + req.url);
+}).listen(PORT, IP_ADDRESS, function () {
+	/*
+	 * サーバ起動時に表示するログ(起動したことが分かりやすい)
+	 */
+	console.log("Server running at http://" + IP_ADDRESS + ":" + PORT + "/");
+	console.log("サーバを終了する際は[ctrl + c]を押してください");
+});
